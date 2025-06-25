f68410a53146bbf9f669d7657362ac0a46eef584
ndietzUCSC <nbdietz@ucsc.edu>
2025-06-24 20:47:21 -0700

    labs push

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,11 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Shuffle the cards thoroughly to randomize their order.  
+// Lay all cards face down on the table.  
+// Player flips two cards per turn.  
+// Check if the flipped cards match.  
+  //If they match, keep the pair.  
+    // Player takes another turn after a match.  
+  // If they do not match, flip the cards back.  
+// Pass the turn to the next player.  
+// Continue until all pairs are matched.  
+// Count each player’s pairs.  
+// Determine the player with the most pairs as the winner.  
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,18 @@
-// index.js - purpose and description here
+// index.js - Lab 4: Variables and Data Types
 // Author: Your Name
-// Date:
+// Date: June 2025
 
-// Constants
+// Variables for car info
+let make = "Toyota"
+let model = "Prius";
+let color = "Silver";
+let year = 2010;
+let age = 2025 - year;
 
-// Functions
+// Reference variables
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
