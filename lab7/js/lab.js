ff3114cfc93174aae6b7bff0bba324b5898ad860
ndietzUCSC <nbdietz@ucsc.edu>
2025-11-01 16:29:10 -0700

    Lab7 sort functional
    formatting and content are next

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -7,15 +7,19 @@
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function sortGuestName() {
+  var guestName = window.prompt("Hello, what's your name?");
+  console.log("guestName = ", guestName);
+  //cast string to array
+  var nameArray = guestName.split('');
+  //sort array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort = ", nameArraySort);
+  //array to string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted = ", nameSorted);
+  return nameSorted; 
 }
+  document.writeln("oh crap I dropped the letters into my sorting machine and they got all...sorted up... See? </br> ",
+  sortGuestName(), "</br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
