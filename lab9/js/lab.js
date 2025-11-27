00a1b21198e0ea8f41885149f73d3335976f3866
ndietzUCSC <nbdietz@ucsc.edu>
2025-11-27 11:35:16 -0800

    Replaced 8 and template folders

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -5,17 +5,25 @@
 // Constants
 
 // Functions
-
+var sortedString;
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function sortGuestName() {
+  var guestName = window.prompt("Hello, what's your name?");
+  console.log("guestName = ", guestName);
+  //cast string to array
+  var nameArray = guestName.split('');
+  //sort array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort = ", nameArraySort);
+  //array to string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted = ", nameSorted); 
+  
+  document.writeln("oh crap I dropped the letters into my sorting machine and they got all...sorted up... See?");
+  sortedString = nameSorted;
 }
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+function namePrint(){
+  document.writeln(sortedString);
 }
+  
 
-// let's get this party started
-main();
