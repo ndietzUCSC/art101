72ce3869156aa0a8c21e86a4269bb2d828336df4
ndietzUCSC <nbdietz@ucsc.edu>
2025-11-27 19:12:53 -0800

    Lab 10 Done

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,26 +4,22 @@
 
 // Constants
 
-// Functions
-var sortedString;
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function sortGuestName() {
-  var guestName = window.prompt("Hello, what's your name?");
-  console.log("guestName = ", guestName);
-  //cast string to array
-  var nameArray = guestName.split('');
-  //sort array
-  var nameArraySort = nameArray.sort();
-  console.log("nameArraySort = ", nameArraySort);
-  //array to string
-  var nameSorted = nameArraySort.join('');
-  console.log("nameSorted = ", nameSorted); 
-  
-  document.writeln("oh crap I dropped the letters into my sorting machine and they got all...sorted up... See?");
-  sortedString = nameSorted;
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
 }
-function namePrint(){
-  document.writeln(sortedString);
-}
+$("#output").append("<button id='make-convo'>Generate Dialogue</button>");
+// click listener for button
+$("#make-convo").click(function(){
+  // get new fake dialogue
+  const newText = generateRandomText();
   
-
+  // append a new div to our output div
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
