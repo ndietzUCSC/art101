eedcaf05c86060168e992a062016a4caca621585
ndietzUCSC <nbdietz@ucsc.edu>
2025-12-10 23:39:21 -0800

    Nav links updated across pages

---
+++ lab15/js/lab.js
@@ -0,0 +1,29 @@
+// index.js - purpose and description here
+// Author: Your Name
+// Date:
+
+// Constants
+
+// Functions
+var sortedString;
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function sortGuestName() {
+  var guestName = window.prompt("Hello, what's your name?");
+  console.log("guestName = ", guestName);
+  //cast string to array
+  var nameArray = guestName.split('');
+  //sort array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort = ", nameArraySort);
+  //array to string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted = ", nameSorted); 
+  
+  document.writeln("oh crap I dropped the letters into my sorting machine and they got all...sorted up... See?");
+  sortedString = nameSorted;
+}
+function namePrint(){
+  document.writeln(sortedString);
+}
+  
+
