c3e2e79654d30bcf3a74e544f9e625b145e0ba1f
ndietzUCSC <nbdietz@ucsc.edu>
2025-12-11 00:16:23 -0800

    Lab 7 repair
    restored missing html in lab 7

---
+++ lab11/lab.js
@@ -0,0 +1,25 @@
+// index.js - Name Sorter
+// Author: Norah Dietz
+// Date:
+
+// Constants
+
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
+$("#output").append("<button id='make-convo'>Generate Dialogue</button>");
+// click listener for button
+$("#make-convo").click(function(){
+  // get new fake dialogue
+  const newText = generateRandomText();
+  
+  // append a new div to our output div
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
