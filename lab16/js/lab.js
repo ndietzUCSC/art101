3a25c28cee64a1d5972762486170af9129dd44b1
ndietzUCSC <nbdietz@ucsc.edu>
2025-12-12 15:38:03 -0800

    xkcd test

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,29 +1,35 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js XKCD API 
+// Author: Norah Dietz
+// Date:12/12/2025
 
 // Constants
 
-// Functions
-var sortedString;
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function sortGuestName() {
-  var guestName = window.prompt("Hello, what's your name?");
-  console.log("guestName = ", guestName);
-  //cast string to array
-  var nameArray = guestName.split('');
-  //sort array
-  var nameArraySort = nameArray.sort();
-  console.log("nameArraySort = ", nameArraySort);
-  //array to string
-  var nameSorted = nameArraySort.join('');
-  console.log("nameSorted = ", nameSorted); 
-  
-  document.writeln("oh crap I dropped the letters into my sorting machine and they got all...sorted up... See?");
-  sortedString = nameSorted;
-}
-function namePrint(){
-  document.writeln(sortedString);
-}
-  
+// Functions 
+
+$('#button').on('click', function () {
+
+  $.ajax({
+    url: 'https://xkcd.com/info.0.json',
+    method: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      comicObj = data;
+
+      // Section title using comicObj.title
+      $('#comic-title').text(comicObj.title);
+
+      // Image using comicObj.img, with alt and title attributes
+      $('#comic-image')
+        .attr('src', comicObj.img)
+        .attr('alt', comicObj.alt)
+        .attr('title', comicObj.alt);
+    },
+    error: function (xhr, status, error) {
+      console.error('AJAX error:', error);
+    },
+    complete: function () {
+      clicked = false;
+    }
+  });
+});
 
